5bd9b697c36ffb1354f1540cfd61c915257ca52b
jeremyw0886 <jeremyw0815@icloud.com>
2025-02-14 11:54:21 -0500

    Optimized -- 5-5_alien_colors_3.py

--- chapter_5/5-5_alien_colors_3.py
+++ chapter_5/5-5_alien_colors_3.py
@@ -1,35 +1,13 @@
-# 5 points for shooting green alien
-alien_color = 'green'
+# Define alien colors and corresponding points
+alien_points = {
+    'green': 5,
+    'yellow': 10,
+    'red': 15
+}
 
-if alien_color == 'green':
-    points = 5
-elif alien_color == 'yellow':
-    points = 10
-else:
-    points = 15
+# List of alien colors
+alien_colors = ['green', 'yellow', 'red']
 
-print(f"You just earned {points} points for shooting a {alien_color} alien!")
-
-# 10 points for shooting yellow alien
-alien_color = 'yellow'
-
-if alien_color == 'green':
-    points = 5
-elif alien_color == 'yellow':
-    points = 10
-else:
-    points = 15
-
-print(f"You just earned {points} points for shooting a {alien_color} alien!")
-
-# 15 points for shooting red alien
-alien_color = 'red'
-
-if alien_color == 'green':
-    points = 5
-elif alien_color == 'yellow':
-    points = 10
-else:
-    points = 15
-
-print(f"You just earned {points} points for shooting a {alien_color} alien!")
+# Loop through the list and print the points
+for color in alien_colors:
+    print(f"You just earned {alien_points[color]} points for shooting a {color} alien!")
