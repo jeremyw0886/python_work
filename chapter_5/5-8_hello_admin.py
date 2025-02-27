bfe8d947d75c004e2bfd0f11a1c2ff40fcfce98c
jeremyw0886 <jeremyw0815@icloud.com>
2025-02-27 16:12:42 -0500

    creates: -- chapter_7 folder completes: -- greeter.py -- parrot.py -- rollercoaster.py -- even_or_odd.py

--- chapter_5/5-8_hello_admin.py
+++ chapter_5/5-8_hello_admin.py
@@ -7,3 +7,19 @@ for username in usernames:
         print("Hello admin, would you like to see a status report?")
     else:
         print(f"Hello {username}, thank you for logging in.")
+
+print('\n')
+
+# List of temperatures
+temps = [17, 32, 65]
+
+# Convert the list into a dictionary with corresponding messages
+temp_messages = {
+    17: "too freakin' cold",
+    32: "freezing but bearable",
+    65: "a nice temperature"
+}
+
+# Print out the messages for each temperature
+for temp in temps:
+    print(f"{temp} degrees is {temp_messages[temp]}")
