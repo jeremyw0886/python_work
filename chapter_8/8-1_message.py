e25bed1265238921e690e614318ec34ed4fb5bb0
jeremyw0886 <jeremyw0815@icloud.com>
2025-03-08 16:34:33 -0500

    completes: -- greeter.py -- 8-1_message.py -- 8-2_favorite_book.py

---
+++ chapter_8/8-1_message.py
@@ -0,0 +1,6 @@
+def display_message():
+    """Print a message about what I'm learning in Chapter 8."""
+    print("I'm learning about functions in this chapter.")
+
+
+display_message()
---
+++ chapter_8/8-2_favorite_book.py
@@ -0,0 +1,6 @@
+def favorite_book(title):
+    """Display a message about someone's favorite book."""
+    print(f"One of my favorite books is {title.title()}.")
+
+
+favorite_book('the great gatsby')
